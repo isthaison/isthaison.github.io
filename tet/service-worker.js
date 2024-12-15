a8e9225cbfae99ef8ace48ef80cb9b4706166d6a
so95 <is.thaison@gmail.com>
2024-12-15 21:55:42 +0700

    Update service-worker.js

--- tet/service-worker.js
+++ tet/service-worker.js
@@ -22,23 +22,31 @@ self.addEventListener("install", (event) => {
 });
 
 // Xử lý fetch request
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response; // Lấy từ cache nếu có
-      }
-      return fetch(event.request)
-        .then((fetchResponse) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, fetchResponse.clone());
-            return fetchResponse;
-          });
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response; // Trả về từ cache nếu có
+            }
+            return fetch(event.request).then(fetchResponse => {
+                if (!fetchResponse.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                // Nếu response có mã trạng thái 206, bỏ qua lưu trữ cache
+                if (fetchResponse.status === 206) {
+                    console.error('Partial response (status code 206) is unsupported');
+                    return fetchResponse;
+                }
+
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            }).catch(error => {
+                console.error('Fetch failed:', error);
+                throw error;
+            });
         })
-        .catch((error) => {
-          console.error("Fetch failed:", error);
-          throw error;
-        });
-    })
-  );
+    );
 });
