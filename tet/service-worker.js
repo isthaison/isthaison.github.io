9902c26d8a7bfb8fcc1801548e3116cb401e4585
so95 <is.thaison@gmail.com>
2024-12-17 07:32:33 +0700

    a

--- tet/service-worker.js
+++ tet/service-worker.js
@@ -5,7 +5,7 @@ const urlsToCache = [
   "/tet/",
   "/tet/manifest.json",
   "/tet/app.css",
-  "/tet/app.hash16.js",
+  "/tet/app.hash17.js",
   "/tet/tet.mp3",
   "/tet/icons/icon-192x192.png",
   "/tet/icons/icon-512x512.png",
