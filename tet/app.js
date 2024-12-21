01c3ed883652e038eb05a15f57128d926f96e411
so95 <is.thaison@gmail.com>
2024-12-21 16:50:14 +0700

    Update app.js

--- tet/app.js
+++ tet/app.js
@@ -422,23 +422,31 @@ function checkForSWUpdate() {
       });
   }
 }
-document.addEventListener("DOMContentLoaded", () => {
-  if ("Notification" in window) {
-    if (Notification.permission !== "granted") {
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          console.log("Thông báo đã được cấp quyền.");
-        } else {
-          console.warn("Quyền thông báo đã bị từ chối.");
-        }
-      });
-    } else if (Notification.permission === "granted") {
-      console.log("Quyền thông báo đã được cấp trước đó.");
-    } else {
-      console.warn("Thông báo không khả dụng do người dùng từ chối.");
-    }
-  } else {
-    console.error("Trình duyệt không hỗ trợ thông báo.");
+
+function notifyMe() {
+  if (!("Notification" in window)) {
+    // Check if the browser supports notifications
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+    const notification = new Notification("Hi there!");
+    // …
+  } else if (Notification.permission !== "denied") {
+    // We need to ask the user for permission
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        const notification = new Notification("Hi there!");
+        // …
+      }
+    });
   }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them anymore.
+}
+document.addEventListener("DOMContentLoaded", () => {
+  notifyMe()
 });
 console.log("(●'◡'●)");
