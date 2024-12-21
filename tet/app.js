486e08e703b24439a2a946159324594c5e356a6b
so95 <is.thaison@gmail.com>
2024-12-21 16:56:37 +0700

    Update app.js

--- tet/app.js
+++ tet/app.js
@@ -426,7 +426,7 @@ function checkForSWUpdate() {
 function notifyMe() {
   if (!("Notification" in window)) {
     // Check if the browser supports notifications
-    alert("This browser does not support desktop notification");
+    //alert("This browser does not support desktop notification");
   } else if (Notification.permission === "granted") {
     // Check whether notification permissions have already been granted;
     // if so, create a notification
