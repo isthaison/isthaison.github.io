95bd06b4be888b8c257c7d12341ac4af8ef84ba3
so95 <is.thaison@gmail.com>
2024-12-21 17:20:52 +0700

    Update app.js

--- tet/app.js
+++ tet/app.js
@@ -430,14 +430,14 @@ function notifyMe() {
   } else if (Notification.permission === "granted") {
     // Check whether notification permissions have already been granted;
     // if so, create a notification
-    const notification = new Notification("Hi there!");
+
     // …
   } else if (Notification.permission !== "denied") {
     // We need to ask the user for permission
     Notification.requestPermission().then((permission) => {
       // If the user accepts, let's create a notification
       if (permission === "granted") {
-        const notification = new Notification("Hi there!");
+      
         // …
       }
     });
